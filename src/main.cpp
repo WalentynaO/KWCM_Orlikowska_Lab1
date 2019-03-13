b1c7f93e74f67cb9c36f90ee2cb59fdfdb564a6e
Orlikowska <student@ib.polsl.pl>
2019-03-13 10:56:03 +0100

    sterowanie procesem, ON

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,26 @@
-#include "trygonometria.h"
+
+	
+#include "LaborkaConfig.h"
 #include <iostream>
+#ifdef USE_TRIGONOMETRY_DEGREE
+#include "trygonometria.h"
 using namespace degreemath;
+#else
+#include <cmath>
+#endif
+
+//
 int main()
 {
+#ifdef USE_TRIGONOMETRY_DEGREE
 std::cout<<degreemath::sin(45);
 std::cout<<degreemath::cos(45);
 std::cout<<degreemath::tg(45);
 std::cout<<degreemath::ctg(45);
+#else
+std::cout<<std::sin(3.14/4);
+std::cout<<std::cos(3.14/4);
+std::cout<<std::tan(3.14/4);
+std::cout<<1/std::tan(3.14/4);
+#endif
 return 0;}
