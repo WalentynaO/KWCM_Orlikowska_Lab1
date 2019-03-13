9528f5bdcd3a0d5971d978322d4ea67d20e12afe
Orlikowska <student@ib.polsl.pl>
2019-03-13 11:17:14 +0100

    testowanie

--- src/Trygonometria/trygonometria.cpp
+++ src/Trygonometria/trygonometria.cpp
@@ -1,18 +1,19 @@
+#define _USE_MATH_DEFINES
 #include "trygonometria.h"
 #include <cmath>
 double degreemath::sin(double degree){
-double radian = 3.14*degree/180;
+double radian = M_PI*degree/180;
 return std::sin(radian);
 }
 double degreemath::cos(double degree){
-double radian = 3.14*degree/180;
+double radian = M_PI*degree/180;
 return std::cos(radian);
 }
 double degreemath::tg(double degree){
-double radian = 3.14*degree/180;
+double radian = M_PI*degree/180;
 return std::tan(radian);
 }
 double degreemath::ctg(double degree){
-double radian = 3.14*degree/180;
+double radian = M_PI*degree/180;
 return 1/std::tan(radian);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,5 @@
 
-	
+#define _USE_MATH_DEFINES	
 #include "LaborkaConfig.h"
 #include <iostream>
 #ifdef USE_TRIGONOMETRY_DEGREE
@@ -18,9 +18,9 @@ std::cout<<degreemath::cos(45);
 std::cout<<degreemath::tg(45);
 std::cout<<degreemath::ctg(45);
 #else
-std::cout<<std::sin(3.14/4);
-std::cout<<std::cos(3.14/4);
-std::cout<<std::tan(3.14/4);
-std::cout<<1/std::tan(3.14/4);
+std::cout<<std::sin(M_PI/4);
+std::cout<<std::cos(M_PI/4);
+std::cout<<std::tan(M_PI/4);
+std::cout<<1/std::tan(M_PI/4);
 #endif
 return 0;}
